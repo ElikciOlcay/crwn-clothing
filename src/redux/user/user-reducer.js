ab10a7dd40257221307db49f6789644ef1afc5c6
Olcay Elikci <olcay.elikci@hotmail.com>
2020-09-25 13:15:24 +0200

    redux

--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -3,13 +3,17 @@ const INITIAL_STATE = {
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
+    // Check to see if the reducer cares about this action
     switch (action.type) {
         case 'SET_CURRENT_USER':
+            // If so, make a copy of `state`
             return {
                 ...state,
+                // and update the copy with the new value
                 currentUser: action.payload
             }
         default:
+            // otherwise return the existing state unchanged
             return state;
     }
 }
