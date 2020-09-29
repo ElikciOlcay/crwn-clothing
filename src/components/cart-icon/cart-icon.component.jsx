8cb56cea9c5652bdee18b2baba0fa39bf7063c69
Olcay Elikci <olcay.elikci@hotmail.com>
2020-09-29 10:55:50 +0200

    updating our app with reselct, replaced cart selectors with momoized selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'; 
 
 import './cart-icon.styles.scss';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 
-const CartIcon = ({toggleCartHidden, quantitiy}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
-<span className="item-count">{quantitiy}</span>
+<span className="item-count">{itemCount}</span>
     </div>
 );
 
@@ -18,8 +19,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = ({cart: { quantitiy }}) => ({
-    quantitiy
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
 });
 
 
