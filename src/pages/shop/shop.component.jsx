83b13e4ab3e9208b78028d367434d056873b885b
Olcay Elikci <olcay.elikci@hotmail.com>
2020-10-01 16:41:41 +0200

    add collections in to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/preview-collection/colection-preview.component';
 import './shop.styles.scss';
 
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super();
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
-        this.state = {
-            collections: SHOP_DATA
+
+
+const ShopPage = ({ collections }) => (
+    <div className="shop-page">
+        {
+            collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
+            ))
         }
+    </div>
+);
 
-    }
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-                    ))
-                }
-            </div>
-        )
-    }
-
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
---
+++ src/redux/directory/directory.selectors.js
@@ -0,0 +1,7 @@
+import { createSelector } from 'reselect';
+const selectDirecotry = state => state.directory;
+
+export const selectDirectorySection = createSelector(
+    [selectDirecotry],
+    directory => directory.sections
+)
