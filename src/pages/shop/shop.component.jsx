785059b08e232c850f93bb126d749188de80fe3b
Olcay Elikci <olcay.elikci@hotmail.com>
2020-10-29 12:14:40 +0100

    change on shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import CollectionPreview from '../../components/preview-collection/collection-preview.component';
->>>>>>> f9c4a19ae9d9d832cb507a36649da58e297a1f95
 import './shop.styles.scss';
 import { Route } from 'react-router-dom';
 
-<<<<<<< HEAD
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collections/collection.component';
-
-
-
-const ShopPage = ({ match }) => (
-    <div className="shop-page">     
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-=======
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 
 
@@ -24,7 +10,6 @@ import CollectionOverview from '../../components/collection-overview/collection-
 const ShopPage = ({ collections }) => (
     <div className="shop-page">
         <CollectionOverview/>
->>>>>>> f9c4a19ae9d9d832cb507a36649da58e297a1f95
     </div>
 );
 
